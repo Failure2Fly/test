80932a3446fe226bc82db758175a0e470f0d6afa
Failure2Fly <failure2fly@gmail.com>
2018-07-28 18:48:26 -0400

    first commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,16 @@
+
+
+import java.util.Scanner;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter a Number");
+			//int a = scanner.nextInt();
+			char c = scanner.next().charAt(0);
+			System.out.println("value =" + c);
+	}
+
+}
